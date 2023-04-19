cc0f56c7db8d102ea62876de8f8f5156bcb5436c
Ruiyan Liang <Jliang@lcs.on.ca>
2023-04-19 10:14:55 -0400

    Initial Commit

---
+++ SongFinder/SongFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SongFinderApp.swift
+//  SongFinder
+//
+//  Created by dyhidrogen monoxide on 2023/4/19.
+//
+
+import SwiftUI
+
+@main
+struct SongFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
